eba0e400d080061075db81c48f1be227698b60de
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 13:48:11 -0300

    create

---
+++ VacinaApp/screens/admin/vacinas/CreateVacinaScreen.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView, Alert } from 'react-native';
+import { DrawerScreenProps } from '@react-navigation/drawer';
+import { DrawerParamList } from '../../../navigation/DrawerNavigator';
+import { API_BASE_URL } from '@/scripts/api'
+
+type Props = DrawerScreenProps<DrawerParamList, 'CreateVacina'>;
+
+const CreateVacinaScreen = ({ navigation }: Props) => {
+  const [nome, setNome] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [fabricante, setFabricante] = useState('');
+  const [dosesRecomendadas, setDosesRecomendadas] = useState('');
+  const API_URL = `${API_BASE_URL}vacinas/`;
+
+  const handleSave = async () => {
+    if (!nome || !fabricante || !dosesRecomendadas) {
+      Alert.alert('Erro', 'Por favor, preencha todos os campos obrigatórios.');
+      return;
+    }
+    const vacinaData = { 
+      nome, 
+      descricao, 
+      fabricante, 
+      doses_recomendadas: parseInt(dosesRecomendadas, 10) 
+    };
+    try {
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(vacinaData),
+      });
+      if (!response.ok) throw new Error('Falha ao criar vacina.');
+      Alert.alert('Sucesso', 'Vacina criada com sucesso!');
+      navigation.navigate('VacinasList');
+    } catch (error) {
+      Alert.alert('Erro', (error as Error).message);
+      console.error(error);
+    }
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.label}>Nome da Vacina</Text>
+      <TextInput style={styles.input} value={nome} onChangeText={setNome} />
+      
+      <Text style={styles.label}>Fabricante</Text>
+      <TextInput style={styles.input} value={fabricante} onChangeText={setFabricante} />
+
+      <Text style={styles.label}>Descrição</Text>
+      <TextInput style={styles.input} value={descricao} onChangeText={setDescricao} multiline />
+
+      <Text style={styles.label}>Doses Recomendadas</Text>
+      <TextInput style={styles.input} value={dosesRecomendadas} onChangeText={setDosesRecomendadas} keyboardType="numeric" />
+
+      <View style={styles.button}>
+        <Button title="Salvar" onPress={handleSave} color="#4CAF50" />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, backgroundColor: 'white' },
+  label: { fontSize: 16, fontWeight: 'bold', marginTop: 12, marginBottom: 4 },
+  input: { borderWidth: 1, borderColor: '#ccc', borderRadius: 8, padding: 12, fontSize: 16 },
+  button: { marginTop: 24 }
+});
+
+export default CreateVacinaScreen;
